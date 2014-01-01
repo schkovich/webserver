b7c9de44e2a36a21f0394447f7af4ec5f834ba7f
Goran Miskovic <goran.v.miskovic@gmail.com>
2014-01-01 15:44:17 +0100

    Ordering creates dependency cycle. :(

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,21 +1,18 @@
 class web_server::nginx (
-  $pool_name,
-  $pool,
-  $client_max_body_size
+$pool_name,
+$pool,
+$client_max_body_size
 ) {
 
   class { '::nginx': }
-
-  nginx::resource::upstream { $pool_name :
-    ensure  => 'present',
-    members => $pool,
-    require => Class['::nginx'],
-    before => File["${nginx::params::nx_conf_dir}/sites-enabled"]
-  }
 
   file {"${nginx::params::nx_conf_dir}/sites-enabled":
     ensure => 'directory',
   }
 
+  nginx::resource::upstream { $pool_name :
+    ensure  => 'present',
+    members => $pool,
+  }
 
 }
