beaf176d5b14208f647d108aa3263708e0ad7dc9
Goran Miskovic <goran.miskovic@timesofmalta.com>
2014-08-29 11:27:32 +0200

    Removing wrong usage of noop metaparameter.

--- manifests/php/extensions/xdebug.pp
+++ manifests/php/extensions/xdebug.pp
@@ -1,17 +1,21 @@
 class web_server::php::extensions::xdebug {
 
-  #  todo: Set ensure on this class instead directly on php::extension::xdebug
+#  todo: Set ensure on this class instead directly on php::extension::xdebug
   include 'php::extension::xdebug'
 
   case $php::extension::xdebug::params::ensure {
-    absent, purged, held: { $noop = false }
-    present, installed, latest: { $noop = true }
-  }
-
-  exec {"sudo php5enmod xdebug":
-    path => ['/usr/bin', '/usr/sbin'],
-    require => Class['php::extension::xdebug'],
-    noop => $noop
+    absent, purged, held: {
+      exec {"sudo php5dismod xdebug":
+        path => ['/usr/bin', '/usr/sbin'],
+        require => Class['php::extension::xdebug'],
+      }
+    }
+    present, installed, latest: {
+      exec {"sudo php5enmod xdebug":
+        path => ['/usr/bin', '/usr/sbin'],
+        require => Class['php::extension::xdebug'],
+      }
+    }
   }
 
 }
