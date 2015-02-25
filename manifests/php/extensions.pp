b2776ac67bb3bc80905d3f915e46352d502f2640
Goran Miskovic <schkovich@gmail.com>
2015-02-25 11:11:46 +0100

    Setting up curl extension.

--- manifests/php/extensions.pp
+++ manifests/php/extensions.pp
@@ -1,4 +1,7 @@
 class web_server::php::extensions {
 
+  package { 'curl': }
+
   include 'web_server::php::extensions::xdebug'
+  include 'web_server::php::extensions::curl'
 }
---
+++ manifests/php/extensions/curl.pp
@@ -0,0 +1,23 @@
+class web_server::php::extensions::curl (
+  $ensure
+) {
+
+  include 'php::extension::curl'
+
+  case $ensure {
+    absent, purged, held: {
+      exec {"sudo php5dismod curl":
+        path => ['/usr/bin', '/usr/sbin'],
+        require => Class['php::extension::curl'],
+      }
+    }
+    present, installed, latest: {
+      exec {"sudo php5enmod curl":
+        path => ['/usr/bin', '/usr/sbin'],
+        require => Class['php::extension::curl'],
+      }
+    }
+  }
+
+  Package['curl'] -> Class['tombox::php::extensions::curl']
+}
