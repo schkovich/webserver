49a5403ac82caae3a6d19d06cde31eeb42f52e6f
Goran Miskovic <schkovich@gmail.com>
2015-02-25 17:02:00 +0100

    Setting up memcache extension.

--- manifests/php/extensions.pp
+++ manifests/php/extensions.pp
@@ -2,4 +2,5 @@ class web_server::php::extensions {
 
   include 'web_server::php::extensions::xdebug'
   include 'web_server::php::extensions::curl'
+  include 'web_server::php::extensions::memcache'
 }
